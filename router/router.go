5087c546ecacb673cd172e483f96283a5d3e071f
Tony-Lucas <tonylucas1997@hotmail.com>
2023-08-09 15:52:44 -0300

    cors

--- router/router.go
+++ router/router.go
@@ -1,31 +1,15 @@
 package router
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
-
-func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
-
-        c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Credentials", "true")
-        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Header("Access-Control-Allow-Methods", "POST,DELETE,GET,PUT")
-
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
-
-        c.Next()
-    }
-}
 
 func Initialize() {
 
 	// Router Instance
 	r := gin.Default()
-	r.Use(CORSMiddleware())
+	r.Use(cors.Default())
 	r.MaxMultipartMemory = 8 << 20
 
 	// Routes
